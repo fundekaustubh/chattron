782a2efab4ac1f4ea43e7921b538261704be3c81
Kaustubh <fundekaustubh@gmail.com>
2023-01-20 10:46:38 +0530

    Basic connection set up. Installed axios and socket client, created the socket events on the backend server, connected the frontend and backend via socket.io.

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,14 +1,30 @@
 import logo from '../../assets/logo.svg';
 import './styles.css';
-
+import { useRef } from 'react';
+import axios from "axios";
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect("http://localhost:8080");
 const App = () => {
+  const socketRef = useRef();
+  socketRef.current = socket;
+  const userName = useRef();
+  const roomName = useRef();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const loginResponse = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/room`, {
+      userName, roomName
+    });
+    console.log("Login response: ", loginResponse);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <form action={process.env.REACT_APP_BACKEND_URL} method="POST">
-          <input type="text" name="name" placeholder="User name" /> <br />
-          <input type="text" name="room" placeholder="Room name" /> <br />
-          <button>Submit</button>
+        <form action={`${process.env.REACT_APP_BACKEND_URL}/room`} method="POST">
+          <input type="text" ref={userName} placeholder="User name" /> <br />
+          <input type="text" ref={roomName} placeholder="Room name" /> <br />
+          <button onClick={handleLogin}>Submit</button>
         </form>
       </header>
     </div>
--- server/app.js
+++ server/app.js
@@ -3,6 +3,37 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express');
 const app = express();
-app.listen(process.env.BACKEND_PORT, () => {
-    console.log(`BACKEND STARTED ON PORT: ${process.env.BACKEND_PORT}`);
+const BACKEND_PORT = process.env.BACKEND_PORT || 8080;
+const http = require('http').Server(app);
+const cors = require('cors');
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Socket IO connection
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: process.env.FRONTEND_URL
+    }
+});
+
+//Add this before the app.get() block
+console.log("Socket .io is: ", socketIO);
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected');
+    });
+});
+
+// Routes
+app.get('/api', (req, res) => {
+    console.log("GET route hit!");
 })
+
+// Server
+http.listen(BACKEND_PORT, () => {
+    console.log(`Server listening on ${BACKEND_PORT}`);
+});
